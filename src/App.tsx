c0faac8a0a14587c962c05fce74c0176d2fbdac6
[dyad] <gustavogama099@gmail.com>
2025-10-13 23:23:17 -0300

    [dyad] Adicionei políticas de RLS temporárias para o bucket `book-pdfs` e iniciei a configuração de autenticação com um `SessionContextProvider`. - wrote 2 file(s), executed 1 SQL queries

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,9 @@ import Goals from "./pages/Goals";
 import Motivation from "./pages/Motivation";
 import Settings from "./pages/Settings";
 import Books from "./pages/Books";
-import BookReader from "./pages/BookReader"; // Importando a nova página BookReader
+import BookReader from "./pages/BookReader";
 import NotFound from "./pages/NotFound";
+import { SessionContextProvider } from "./integrations/supabase/auth"; // Importar o provedor de sessão
 
 const queryClient = new QueryClient();
 
@@ -21,19 +22,21 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/tasks" element={<Tasks />} />
-            <Route path="/goals" element={<Goals />} />
-            <Route path="/motivation" element={<Motivation />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/books" element={<Books />} />
-            <Route path="/books/:id" element={<BookReader />} /> {/* Nova rota para o leitor de livros */}
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <SessionContextProvider> {/* Envolver o aplicativo com o provedor de sessão */}
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/tasks" element={<Tasks />} />
+              <Route path="/goals" element={<Goals />} />
+              <Route path="/motivation" element={<Motivation />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/books" element={<Books />} />
+              <Route path="/books/:id" element={<BookReader />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </SessionContextProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
