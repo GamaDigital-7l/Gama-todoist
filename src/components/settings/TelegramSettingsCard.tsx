0013ed4ff9e84dd90a38584d25795f88fd108f40
[dyad] <gustavogama099@gmail.com>
2025-10-17 15:31:58 -0300

    [dyad] Corrigido o erro de sintaxe JSX no componente TelegramSettingsCard escapando as chaves. - wrote 1 file(s)

--- src/components/settings/TelegramSettingsCard.tsx
+++ src/components/settings/TelegramSettingsCard.tsx
@@ -124,7 +124,7 @@ const TelegramSettingsCard: React.FC<TelegramSettingsCardProps> = ({ userId, ses
                   <br />
                   `https://api.telegram.org/bot[SEU_TOKEN_DO_BOT]/getUpdates`
                   <br />
-                  Procure por <code>"chat":{"id":...}</code>
+                  Procure por <code>"chat":&lcub;"id":...&rcub;</code>
                 </p>
               </div>
               <Button
