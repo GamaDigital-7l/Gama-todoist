e96010c3c431bad968f2a4a847b8f41086450029
[dyad] <gustavogama099@gmail.com>
2025-10-18 12:15:46 -0300

    [dyad] Adicionadas flags futuras do React Router e explicados avisos do console - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ if ('serviceWorker' in navigator) {
 }
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
     <App />
   </BrowserRouter>
 );
